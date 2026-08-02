de669de49e27be19ba43cda2bc87292df3d98049
THE A TEAM R&H <trusttheateam@gmail.com>
2026-08-03 10:32:30 +1200

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,1 +1,24 @@
-<script src="/analytics.js" defer></script>
+function loadGoogleAnalytics() {
+  if (window.analyticsLoaded) return;
+  window.analyticsLoaded = true;
+
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = "https://googletagmanager.com";
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-88WTQFKQV6');
+  
+  console.log('Google Analytics loaded via user interaction!');
+}
+
+const interactionEvents = ['mouseover', 'keydown', 'touchstart', 'scroll'];
+interactionEvents.forEach(event => {
+  window.addEventListener(event, loadGoogleAnalytics, { once: true });
+});
+
+setTimeout(loadGoogleAnalytics, 4000);
+
